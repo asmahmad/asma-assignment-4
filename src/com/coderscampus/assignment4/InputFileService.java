543d184160aabc7e0ff93740840c7934158d42aa
asmahmad <121257988+asmahmad@users.noreply.github.com>
2023-02-03 20:51:17 -0800

    Final Commit with Arrays Updated

--- src/com/coderscampus/assignment4/InputFileService.java
+++ src/com/coderscampus/assignment4/InputFileService.java
@@ -16,17 +16,19 @@ public class InputFileService {
 				fileReader = new BufferedReader(new FileReader("student-master-list.csv"));
 				fileReader.readLine();
 				int i=0;
+				Courses course = null;
+				
 				while ((line = fileReader.readLine()) != null) {
 					String[] dataInEachField = line.split(",");
 					if( (dataInEachField[2].matches("^COMPSCI+ [0-9]+")) ){
-						Courses course1 = new Courses(Integer.parseInt(dataInEachField[0]), dataInEachField[1],dataInEachField[2],Integer.parseInt(dataInEachField[3]));
-						course1Record[i] = course1;
+						course = new Courses(Integer.parseInt(dataInEachField[0]), dataInEachField[1],dataInEachField[2],Integer.parseInt(dataInEachField[3]));
+						course1Record[i] = course;
 					}else if(( (dataInEachField[2].matches("^APMTH+ [0-9]+")) )){
-						Courses course2 = new Courses(Integer.parseInt(dataInEachField[0]), dataInEachField[1],dataInEachField[2],Integer.parseInt(dataInEachField[3]));
-						course2Record[i] = course2;
+						course = new Courses(Integer.parseInt(dataInEachField[0]), dataInEachField[1],dataInEachField[2],Integer.parseInt(dataInEachField[3]));
+						course2Record[i] = course;
 					}else if(( (dataInEachField[2].matches("^STAT+ [0-9]+")) )){
-						Courses course3 = new Courses(Integer.parseInt(dataInEachField[0]), dataInEachField[1],dataInEachField[2],Integer.parseInt(dataInEachField[3]));
-						course3Record[i] = course3;
+						course = new Courses(Integer.parseInt(dataInEachField[0]), dataInEachField[1],dataInEachField[2],Integer.parseInt(dataInEachField[3]));
+						course3Record[i] = course;
 					}
 					i++;
 				}
